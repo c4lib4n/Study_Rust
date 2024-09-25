313fb8f35b07915aa407da154d86f996e05f7282
chagasda <matheussilva_51@hotmail.com>
2024-09-25 10:03:23 +0100

    Add new examplo for course 2

---
+++ Rust_Course_2/Hello_World/target/debug/Hello_World.d
@@ -0,0 +1,1 @@
+C:\Users\chagasda\Documents\Study\Study_Rust\Rust_Course_2\Hello_World\target\debug\Hello_World.exe: C:\Users\chagasda\Documents\Study\Study_Rust\Rust_Course_2\Hello_World\src\main.rs
---
+++ Rust_Course_2/Hello_World/target/debug/deps/Hello_World.d
@@ -0,0 +1,5 @@
+C:\Users\chagasda\Documents\Study\Study_Rust\Rust_Course_2\Hello_World\target\debug\deps\Hello_World.exe: src/main.rs
+
+C:\Users\chagasda\Documents\Study\Study_Rust\Rust_Course_2\Hello_World\target\debug\deps\Hello_World.d: src/main.rs
+
+src/main.rs:
