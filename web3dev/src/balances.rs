9f18c5d083a220ae6357f4567e3d9b809bdd6d94
Matheus Silva <matheussilva_51@hotmail.com>
2024-12-16 14:46:19 +0000

    Section 2 start

--- web3dev/src/balances.rs
+++ web3dev/src/balances.rs
@@ -10,12 +10,30 @@ impl Pallet {
         }
     }
 
-    pub fn set_balance(&mut self, who: &String, amount: u128) {
-        self.balances.insert(who.clone(), amount);
+    pub fn set_balance(&mut self, caller: &String, amount: u128) {
+        self.balances.insert(caller.clone(), amount);
     }
 
     pub fn balance(&self, who: &String) -> u128 {
         *self.balances.get(who).unwrap_or(&0)
+    }
+
+    pub fn transfer(
+        &mut self,
+        caller: String,
+        to: String,
+        amount: u128,
+    ) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
+
+        let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough balance")?;
+        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+
+        self.balances.insert(caller, new_caller_balance);
+        self.balances.insert(to, new_to_balance);
+
+        Ok(())
     }
 }
 
@@ -30,4 +48,21 @@ mod tests {
         assert_eq!(balances.balance(&"Alice".to_string()), 100);
         assert_eq!(balances.balance(&"Bob".to_string()), 0);
     }
+}
+
+#[test]
+fn transfer_balance(){
+    let mut balances = Pallet::new();
+
+    assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 10), Err("Not enough balance"));
+    balances.set_balance(&"Alice".to_string(), 10);
+    assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 5), Ok(()));
+
+    assert_eq!(balances.balance(&"Alice".to_string()), 5);
+    assert_eq!(balances.balance(&"Bob".to_string()), 5);
+
+    balances.set_balance(&"Bob".to_string(), u128::MAX);
+    assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 5), Err("Overflow"));
+
+
 }
--- web3dev/src/main.rs
+++ web3dev/src/main.rs
@@ -1,13 +1,15 @@
 mod balances;
+mod system;
+
 use std::collections::BTreeMap;
 use crate::balances::Pallet;
 
 fn main() {
     let mut pallet = Pallet::new();
-    pallet.set_balance(&"Matheus".to_string(), 10);
+    pallet.set_balance(&"Matheus".to_string(), 20);
 
     let balance = pallet.balance(&"Matheus".to_string());
-    println!("Balance: {}", balance);
+   //println!("Balance: {}", balance);
 
 
 
