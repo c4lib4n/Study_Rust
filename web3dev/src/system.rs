5f5dc8c50a1c6abdc4b55fc853832e6e04c3ed99
Matheus Silva <matheussilva_51@hotmail.com>
2024-12-17 16:57:12 +0000

    Finished Section 2 and Started Section 3. Done until 03.04 module

--- web3dev/src/system.rs
+++ web3dev/src/system.rs
@@ -1,28 +1,37 @@
 use std::collections::BTreeMap;
+use num::traits::{Zero, One};
+use core::ops::AddAssign;
 
-pub struct Pallet {
-    block_number: u32,
-    nonce: BTreeMap<String, u32>,
+#[derive(Debug)]
+pub struct Pallet<BlockNumber, AccountId, Nonce> {
+    block_number: BlockNumber,
+    nonce: BTreeMap<AccountId, Nonce>,
 }
 
-impl Pallet {
-    pub fn new() -> Pallet {
+impl < BlockNumber, AccountId, Nonce>Pallet<BlockNumber, AccountId, Nonce>
+where
+    BlockNumber: Zero + One + AddAssign + Copy,
+    AccountId: Ord + Clone,
+    Nonce: Zero + One + Copy,
+
+{
+    pub fn new() -> Self {
         Pallet {
-            block_number: 0,
+            block_number: BlockNumber::zero(),
             nonce: BTreeMap::new(),
         }
     }
 
-    pub fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> BlockNumber {
         self.block_number
     }
 
     pub fn inc_block_number(&mut self) {
-        self.block_number += 1;
+        self.block_number += BlockNumber::one();
     }
 
-    pub fn inc_nonce(&mut self, account: &String) {
-        let nonce = self.nonce.get(account).unwrap_or(&0) + 1;
+    pub fn inc_nonce(&mut self, account: &AccountId) {
+        let nonce = *self.nonce.get(account).unwrap_or(&Nonce::zero()) + Nonce::one();
         self.nonce.insert(account.clone(), nonce);
     }
 }
@@ -31,7 +40,8 @@ impl Pallet {
 mod test {
     #[test]
     fn init_system(){
-        let mut system = super::Pallet::new();
+        let mut system = super::Pallet::<u32, String, u128>::new();
+
         assert_eq!(system.block_number(), 0);
         assert_eq!(system.nonce.get(&"Alice".to_string()), None);
 
@@ -43,6 +53,27 @@ mod test {
 
 
     }
+
+    #[test]
+        fn increment_block_number(){
+        let mut system = super::Pallet::<u32, String, u128>::new();
+
+        assert_eq!(system.block_number(), 0);
+        system.inc_block_number();
+        assert_eq!(system.block_number(), 1);
+    }
+
+    #[test]
+        fn increment_nonce(){
+        let mut system = super::Pallet::<u32, String, u128>::new();
+
+        system.inc_nonce(&"Alice".to_string());
+        assert_eq!(system.nonce.get(&"Alice".to_string()).unwrap(), &1);
+        system.inc_nonce(&"Alice".to_string());
+        assert_eq!(system.nonce.get(&"Alice".to_string()).unwrap(), &2);
+    }
+
+
 }
 
 
