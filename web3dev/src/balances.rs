05429e82a64e9f8b1f06a501cc22366b796259ea
matheusSilva <matheussilva_51@hotmail.com>
2025-01-02 10:44:57 -0300

    Finished Section 3 for web3dev course.

--- web3dev/src/balances.rs
+++ web3dev/src/balances.rs
@@ -1,18 +1,16 @@
 use std::collections::BTreeMap;
 use num::traits::{CheckedAdd, CheckedSub, Zero};
 
-
-#[derive(Debug)]
-pub struct Pallet<AccountId, Balance> {
-    balances: BTreeMap<AccountId, Balance>,
+pub trait Config: crate::system::Config{
+    type Balance: CheckedAdd + CheckedSub + Zero + Copy;
 }
 
-impl <AccountId, Balance> Pallet <AccountId, Balance>
+#[derive(Debug)]
+pub struct Pallet<T:Config> {
+    balances: BTreeMap<T::AccountId, T::Balance>,
+}
 
-where
-AccountId: Ord + Clone,
-Balance: CheckedAdd + CheckedSub + Zero + Copy,
-
+impl <T:Config> Pallet <T>
 {
     pub fn new() -> Self {
         Self {
@@ -20,19 +18,19 @@ Balance: CheckedAdd + CheckedSub + Zero + Copy,
         }
     }
 
-    pub fn set_balance(&mut self, caller: &AccountId, amount: Balance) {
+    pub fn set_balance(&mut self, caller: &T::AccountId, amount: T::Balance) {
         self.balances.insert(caller.clone(), amount);
     }
 
-    pub fn balance(&self, who: &AccountId) -> Balance {
-        *self.balances.get(who).unwrap_or(&Balance::zero())
+    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
+        *self.balances.get(who).unwrap_or(&T::Balance::zero())
     }
 
     pub fn transfer(
         &mut self,
-        caller: AccountId,
-        to: AccountId,
-        amount: Balance,
+        caller: T::AccountId,
+        to: T::AccountId,
+        amount: T::Balance,
     ) -> Result<(), &'static str> {
         let caller_balance = self.balance(&caller);
         let to_balance = self.balance(&to);
@@ -50,29 +48,42 @@ Balance: CheckedAdd + CheckedSub + Zero + Copy,
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    struct TestConfig;
+
+    impl super::Config for TestConfig { 
+        type Balance = u128;
+    }
+
+    impl crate::system::Config for TestConfig {
+        type AccountId = String;
+        type BlockNumber = u32;
+        type Nonce = u32;
+        
+    }
+
     #[test]
     fn init_balance() {
-        let mut balances = Pallet::<String, u128>::new();
+        let mut balances = Pallet::<TestConfig>::new();
         assert_eq!(balances.balance(&"Alice".to_string()), 0);
         balances.set_balance(&"Alice".to_string(), 100);
         assert_eq!(balances.balance(&"Alice".to_string()), 100);
         assert_eq!(balances.balance(&"Bob".to_string()), 0);
     }
-}
-
-#[test]
-fn transfer_balance(){
-    let mut balances = Pallet::<String, u128>::new();
-
-    assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 10), Err("Not enough balance"));
-    balances.set_balance(&"Alice".to_string(), 10);
-    assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 5), Ok(()));
-
-    assert_eq!(balances.balance(&"Alice".to_string()), 5);
-    assert_eq!(balances.balance(&"Bob".to_string()), 5);
-
-    balances.set_balance(&"Bob".to_string(), u128::MAX);
-    assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 5), Err("Overflow"));
 
 
+    #[test]
+    fn transfer_balance() {
+        let mut balances = Pallet::<TestConfig>::new();
+
+        assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 10), Err("Not enough balance"));
+        balances.set_balance(&"Alice".to_string(), 10);
+        assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 5), Ok(()));
+
+        assert_eq!(balances.balance(&"Alice".to_string()), 5);
+        assert_eq!(balances.balance(&"Bob".to_string()), 5);
+
+        balances.set_balance(&"Bob".to_string(), u128::MAX);
+        assert_eq!(balances.transfer("Alice".to_string(), "Bob".to_string(), 5), Err("Overflow"));
+    }
 }
--- web3dev/src/system.rs
+++ web3dev/src/system.rs
@@ -2,45 +2,56 @@ use std::collections::BTreeMap;
 use num::traits::{Zero, One};
 use core::ops::AddAssign;
 
+
+pub trait Config{
+    type AccountId: Ord + Clone;
+    type BlockNumber: Zero + One + AddAssign + Copy;
+    type Nonce: Zero + One + Copy;
+}
 #[derive(Debug)]
-pub struct Pallet<BlockNumber, AccountId, Nonce> {
-    block_number: BlockNumber,
-    nonce: BTreeMap<AccountId, Nonce>,
+pub struct Pallet<T:Config> {
+    block_number: T::BlockNumber,
+    nonce: BTreeMap<T::AccountId, T::Nonce>,
 }
 
-impl < BlockNumber, AccountId, Nonce>Pallet<BlockNumber, AccountId, Nonce>
-where
-    BlockNumber: Zero + One + AddAssign + Copy,
-    AccountId: Ord + Clone,
-    Nonce: Zero + One + Copy,
+impl < T:Config> Pallet <T>
 
 {
     pub fn new() -> Self {
         Pallet {
-            block_number: BlockNumber::zero(),
+            block_number: T::BlockNumber::zero(),
             nonce: BTreeMap::new(),
         }
     }
 
-    pub fn block_number(&self) -> BlockNumber {
+    pub fn block_number(&self) -> T::BlockNumber {
         self.block_number
     }
 
     pub fn inc_block_number(&mut self) {
-        self.block_number += BlockNumber::one();
+        self.block_number += T::BlockNumber::one();
     }
 
-    pub fn inc_nonce(&mut self, account: &AccountId) {
-        let nonce = *self.nonce.get(account).unwrap_or(&Nonce::zero()) + Nonce::one();
+    pub fn inc_nonce(&mut self, account: &T::AccountId) {
+        let nonce = *self.nonce.get(account).unwrap_or(&T::Nonce::zero()) + T::Nonce::one();
         self.nonce.insert(account.clone(), nonce);
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::system::Config;
+
+    struct TestConfig;
+
+    impl super::Config for TestConfig{
+        type AccountId = String;
+        type BlockNumber = u32;
+        type Nonce = u32;
+    }
     #[test]
     fn init_system(){
-        let mut system = super::Pallet::<u32, String, u128>::new();
+        let mut system = super::Pallet::<TestConfig>::new();
 
         assert_eq!(system.block_number(), 0);
         assert_eq!(system.nonce.get(&"Alice".to_string()), None);
@@ -56,7 +67,7 @@ mod test {
 
     #[test]
         fn increment_block_number(){
-        let mut system = super::Pallet::<u32, String, u128>::new();
+        let mut system = super::Pallet::<TestConfig>::new();
 
         assert_eq!(system.block_number(), 0);
         system.inc_block_number();
@@ -65,7 +76,7 @@ mod test {
 
     #[test]
         fn increment_nonce(){
-        let mut system = super::Pallet::<u32, String, u128>::new();
+        let mut system = super::Pallet::<TestConfig>::new();
 
         system.inc_nonce(&"Alice".to_string());
         assert_eq!(system.nonce.get(&"Alice".to_string()).unwrap(), &1);
