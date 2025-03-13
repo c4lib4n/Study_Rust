39e54e45c965d5ba6300275d2f1f94db7c96b320
Matheus SIlva <matheussilva_51@hotmail.com>
2025-03-13 12:32:23 +0000

    Build with Web3dev Rust program finished.

--- web3dev/src/balances.rs
+++ web3dev/src/balances.rs
@@ -10,21 +10,8 @@ pub struct Pallet<T: Config> {
     balances: BTreeMap<T::AccountId, T::Balance>,
 }
 
+#[macros::call]
 impl<T: Config> Pallet<T> {
-    pub fn new() -> Self {
-        Self {
-            balances: BTreeMap::new(),
-        }
-    }
-
-    pub fn set_balance(&mut self, caller: &T::AccountId, amount: T::Balance) {
-        self.balances.insert(caller.clone(), amount);
-    }
-
-    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
-        *self.balances.get(who).unwrap_or(&T::Balance::zero())
-    }
-
     pub fn transfer(
         &mut self,
         caller: T::AccountId,
@@ -46,25 +33,19 @@ impl<T: Config> Pallet<T> {
     }
 }
 
-pub enum Call<T: Config> {
-    Transfer {
-        to: T::AccountId,
-        amount: T::Balance,
-    },
-}
-
-impl<T: Config> crate::support::Dispatch for Pallet<T> {
-    type Caller = T::AccountId;
-    type Call = Call<T>;
-
-    fn dispatch(
-        &mut self,
-        caller: Self::Caller,
-        call: Self::Call,
-    ) -> crate::support::DispatchResult {
-        match call {
-            Call::Transfer { to, amount } => self.transfer(caller, to, amount),
+impl<T: Config> Pallet<T> {
+    pub fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
         }
+    }
+
+    pub fn set_balance(&mut self, caller: &T::AccountId, amount: T::Balance) {
+        self.balances.insert(caller.clone(), amount);
+    }
+
+    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
+        *self.balances.get(who).unwrap_or(&T::Balance::zero())
     }
 }
 
--- web3dev/src/main.rs
+++ web3dev/src/main.rs
@@ -14,18 +14,13 @@ mod types {
     pub type Block = support::Block<Header, Extrinsic>;
     pub type Content = &'static str;
 }
-
-pub enum RuntimeCall {
-    Balances(balances::Call<Runtime>),
-    ProofOfExistence(proof_of_existence::Call<Runtime>),
-}
 #[derive(Debug)]
+#[macros::runtime]
 pub struct Runtime {
-    balances: balances::Pallet<Self>,
     system: system::Pallet<Self>,
-    proof_of_existence: proof_of_existence::Pallet<Runtime>,
+    balances: balances::Pallet<Self>,
+    proof_of_existence: proof_of_existence::Pallet<Self>,
 }
-
 impl balances::Config for Runtime {
     type Balance = types::Balance;
 }
@@ -39,56 +34,6 @@ impl proof_of_existence::Config for Runtime {
     type Content = types::Content;
 }
 
-impl Runtime {
-    pub fn new() -> Self {
-        Runtime {
-            balances: balances::Pallet::new(),
-            system: system::Pallet::new(),
-            proof_of_existence: proof_of_existence::Pallet::new(),
-        }
-    }
-    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
-        self.system.inc_block_number();
-        if (self.system.block_number() != block.header.block_number) {
-            return Err("Block number dismatch");
-        }
-
-        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
-            self.system.inc_nonce(&caller);
-            let _ = self.dispatch(caller, call).map_err(|e| {
-                eprintln!(
-                    "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
-                    block.header.block_number, i, e
-                )
-            });
-        }
-        Ok(())
-    }
-}
-impl crate::support::Dispatch for Runtime {
-    type Caller = <Runtime as system::Config>::AccountId;
-    type Call = RuntimeCall;
-    // Dispatch a call on behalf of a caller. Increments the caller's nonce.
-    //
-    // Dispatch allows us to identify which underlying module call we want to execute.
-    // Note that we extract the `caller` from the extrinsic, and use that information
-    // to determine who we are executing the call on behalf of.
-    fn dispatch(
-        &mut self,
-        caller: Self::Caller,
-        runtime_call: Self::Call,
-    ) -> support::DispatchResult {
-        match runtime_call {
-            RuntimeCall::Balances (call) => {
-                self.balances.dispatch(caller, call)?;
-            },
-            RuntimeCall::ProofOfExistence(call) => {
-                self.proof_of_existence.dispatch(caller, call)?;
-            }
-        }
-        Ok(())
-    }
-}
 
 use crate::support::Dispatch;
 use crate::system::Config;
@@ -108,11 +53,11 @@ fn main() {
         extrinsics: vec![
             support::Extrinsic{
                 caller: alice.clone(),
-                call: RuntimeCall::Balances(balances::Call::Transfer  {to: bob.clone(), amount: 30}),
+                call: RuntimeCall::balances(balances::Call::transfer  {to: bob.clone(), amount: 30}),
             },
             support::Extrinsic{
                 caller: alice.clone(),
-                call: RuntimeCall::Balances (balances::Call::Transfer {to: charlie.clone(), amount: 20}),
+                call: RuntimeCall::balances(balances::Call::transfer {to: charlie.clone(), amount: 20}),
             },
         ],
     };
@@ -123,15 +68,15 @@ fn main() {
         extrinsics: vec![
             support::Extrinsic{
                 caller: alice,
-                call: RuntimeCall::ProofOfExistence(proof_of_existence::Call::CreateClaim  {claim: "my_first_claim"}),
+                call: RuntimeCall::proof_of_existence(proof_of_existence::Call::create_claim  {claim: "my_first_claim"}),
             },
             support::Extrinsic{
                 caller: bob.clone(),
-                call: RuntimeCall::ProofOfExistence (proof_of_existence::Call::CreateClaim {claim: "Bobs claim"}),
+                call: RuntimeCall::proof_of_existence(proof_of_existence::Call::create_claim {claim: "Bobs claim"}),
             },
             support::Extrinsic{
                 caller: bob.clone(),
-                call: RuntimeCall::ProofOfExistence (proof_of_existence::Call::RevokeClaim {claim: "my_first_claim"}),
+                call: RuntimeCall::proof_of_existence(proof_of_existence::Call::revoke_claim {claim: "my_first_claim"}),
             },
         ],
     };
